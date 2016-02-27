0cdb10b8dcac25283f4a4dec7a8a791452ea55e2
code1up <dev.code1up@googlemail.com>
2016-02-27 11:10:34 +0000

    fix quotes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-var UI = require('ui');
-var Vector2 = require('vector2');
+var UI = require("ui");
+var Vector2 = require("vector2");
 
 var main = new UI.Card({
-    title: 'RAG Status',
-    icon: 'images/menu_icon.png',
-    subtitle: 'Hello World!',
-    body: 'Press any button.',
-    subtitleColor: 'indigo', // Named colors
-    bodyColor: '#9a0036' // Hex colors
+    title: "RAG Status",
+    icon: "images/menu_icon.png",
+    subtitle: "Hello World!",
+    body: "Press any button.",
+    subtitleColor: "indigo", // Named colors
+    bodyColor: "#9a0036" // Hex colors
 });
 
 main.show();
 
-main.on('click', 'select', function (e) {
+main.on("click", "select", function (e) {
 });
